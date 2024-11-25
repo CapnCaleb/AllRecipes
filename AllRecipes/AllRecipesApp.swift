611ffab3bdf82a41603c7146ac54f57fa3f36779
Caleb Hermann <caleb.hermann@gmail.com>
2024-11-25 11:48:33 -0700

    Initial Commit

---
+++ AllRecipes/AllRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllRecipesApp.swift
+//  AllRecipes
+//
+//  Created by Caleb on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AllRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
